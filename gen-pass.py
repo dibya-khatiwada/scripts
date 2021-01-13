3fd225c831bf552a70dae9e920f9fdbf0ebbf8ff
Dibya Khatiwada <dibya@nren.net.np>
2021-01-13 14:39:04 +0545

    Create gen-pass.py

---
+++ gen-pass.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import argparse
+codeword = []
+parser = argparse.ArgumentParser(usage="python gen-password.py -l <length of the password>")
+parser.add_argument( '-l', '--length', type=int, help='Length of the password', required=False)
+parser.add_argument( '-s', '--steps', type=int, default=1, help='Steps to repeat for password generation', required=False)
+
+args = parser.parse_args()
+
+def get_random_string(length):
+    cmd = 'LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)\[\]-+= < /dev/urandom | head -c '+str(length)+' | xargs'
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None, shell=True)
+    output = process.communicate()
+    return (output[0].strip().decode())
+
+if args.length and args.steps:
+    for x in range(args.steps):
+        codeword.append(get_random_string(args.length))
+
+print("-".join(codeword))
+
